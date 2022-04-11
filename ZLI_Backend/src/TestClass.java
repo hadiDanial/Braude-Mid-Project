24a2a87838a26d7ff43209423d380327a2d24df8
Hadi <hadiDanial@users.noreply.github.com>
2022-04-12 01:45:36 +0300

    Added Shared Project to class path for backend and frontend

---
+++ ZLI_Backend/src/TestClass.java
@@ -0,0 +1,12 @@
+import entities.Product;
+import enums.ProductType;
+
+public class TestClass
+{
+
+	public static void main(String[] args)
+	{
+		Product product = new Product(0,"Test", 10, null, 5, ProductType.BridalBouquet);
+		System.out.println("Back end: " + product);
+	}
+}
---
+++ ZLI_Frontend/src/TestClass.java
@@ -0,0 +1,13 @@
+import entities.Product;
+import enums.ProductType;
+
+public class TestClass
+{
+
+	public static void main(String[] args)
+	{
+		Product product = new Product(0,"Test", 10, null, 5, ProductType.BridalBouquet);
+		System.out.println("Front end: " + product);
+	}
+
+}
