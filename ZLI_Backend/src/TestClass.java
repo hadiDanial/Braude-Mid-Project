e6de0ce8477e5467d5b26eaf0a59f822ff9e0bbf
Hadi <hadiDanial@users.noreply.github.com>
2022-04-12 09:55:41 +0300

    Updated TestClasses

--- ZLI_Backend/src/TestClass.java
+++ ZLI_Backend/src/TestClass.java
@@ -6,7 +6,7 @@ public class TestClass
 
 	public static void main(String[] args)
 	{
-		Product product = new Product(0,"Test", 10, null, 5, ProductType.BridalBouquet);
+		Product product = new Product("Test", 10, null, 5, ProductType.BridalBouquet);
 		System.out.println("Back end: " + product);
 	}
 }
