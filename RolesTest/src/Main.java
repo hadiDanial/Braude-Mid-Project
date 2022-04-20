f6dd43f9ad658d7702be5cf24a9129de5d52da19
Hadi <hadiDanial@users.noreply.github.com>
2022-04-20 22:47:36 +0300

    Deleted extra file

--- RolesTest/src/Main.java
+++ RolesTest/src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main
 {
 
-	public static void main(String[] args)
+	public static void mainTest(String[] args)
 	{
 		Controller controller = new Controller();
 		User u1 = new User("Manager", Role.Manager);   
