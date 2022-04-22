62f3dfd5d133c0762e8058e2347656bf1e7ff806
Hadi <hadiDanial@users.noreply.github.com>
2022-04-22 11:42:47 +0300

    Generate Orders Table in OrdersPage

--- ZLI_Frontend/src/client/ClientProperties.java
+++ ZLI_Frontend/src/client/ClientProperties.java
@@ -4,7 +4,8 @@ public class ClientProperties
 {
 	private static String hostAddress = "localhost";
 	private static int hostPort = 5555;
-	
+	private static int clientWidth = 1240; 
+	private static int clientHeight = 800; 
 	public static String getHostAddress()
 	{
 		return hostAddress;
@@ -20,6 +21,14 @@ public class ClientProperties
 	public static void setHostPort(int hostPort)
 	{
 		ClientProperties.hostPort = hostPort;
+	}
+	public static int getClientWidth()
+	{
+		return clientWidth;
+	}
+	public static int getClientHeight()
+	{
+		return clientHeight;
 	} 
 	
 	
