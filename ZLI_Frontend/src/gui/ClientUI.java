7a9598525185a3e5b6d72b311be5b16c9530105f
Hadi <hadiDanial@users.noreply.github.com>
2022-04-22 11:40:09 +0300

    ClientUI, ClientController
    ClientController now has a reference to the client UI, and has functions for getting and updating the root/stage
    Added Update Scene Root function to ClientUI
    Added ClientUI.fxml

--- ZLI_Frontend/src/gui/ClientUI.java
+++ ZLI_Frontend/src/gui/ClientUI.java
@@ -3,18 +3,23 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 
+import controllers.ClientController;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class ClientUI extends Application
 {
-	Stage window;
-
+	private static Stage window;
+	private static Scene scene;
+	private static AnchorPane root;
 	public static void main(String[] args)
 	{
 		launch(args);
@@ -26,19 +31,44 @@ public class ClientUI extends Application
 	 */
 	public void start(Stage primaryStage)
 	{
+		root = new AnchorPane();
 		window = primaryStage;
+		ClientController.setClientUI(this);
 		AnchorPane anchor;
 		try
 		{
-			anchor = FXMLLoader.load(getClass().getResource("/gui/orders/OrdersPage.fxml"));
-			Scene scene = new Scene(anchor);
-			window.setTitle("Orders");
-			window.setScene(scene);
-			window.show();
+			anchor = FXMLLoader.load(getClass().getResource("/gui/client/ClientUI.fxml"));
+			updateSceneRoot(anchor, "ZLI");
 		} catch (IOException e1)
 		{
 			e1.printStackTrace();
 		}
+	}
+	public void updateSceneRoot(Node newRoot, String newTitle)
+	{
+		root = new AnchorPane();
+		root.getChildren().clear();
+		root.getChildren().add(newRoot);
+		scene = new Scene(root);
+//		window.close();
+		window.setTitle(newTitle);
+		window.setScene(scene);
+		window.show();
+	}
+	public Stage getWindow()
+	{
+		return window;
+	}
+	
+	public void onSettingsButtonClicked(ActionEvent event) throws IOException
+	{
+		AnchorPane anchor = FXMLLoader.load(getClass().getResource("/gui/orders/OrdersPage.fxml"));
+		updateSceneRoot(anchor, "Orders");
+	}
+
+	public Pane getRoot()
+	{
+		return root;
 	}
 
 }
