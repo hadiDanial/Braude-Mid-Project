c16910535d6d919470ebf578bdeba6fcdf15f8e0
Yosef Awad <yosef.awad.1@gmail.com>
2022-04-24 02:47:58 +0300

    deleted old server

--- ZLI_Backend/src/database/MySQLConnection.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public class MySQLConnection extends DatabaseConnection
-{
-
-	protected String driverClassName = "com.mysql.cj.jdbc.Driver";
-	protected String dbURLPrefix = "jdbc:mysql://";
-
-}
