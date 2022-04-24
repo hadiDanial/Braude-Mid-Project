4b6e33cd88381aedb2419dde61d338ca12fea919
Hadi <hadiDanial@users.noreply.github.com>
2022-04-24 18:48:46 +0300

    Changed datetime to timestamp

--- OrderQueries.sql
+++ OrderQueries.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS `Orders`;
 create table `Orders`(`orderNumber` int primary key AUTO_INCREMENT, `price` float, `greetingCard` varchar(256), `color` varchar(32), 
-					  `dOrder` varchar(256), `shop` varchar(32), `date` datetime, `orderDate` datetime);
+					  `dOrder` varchar(256), `shop` varchar(32), `date` timestamp, `orderDate` timestamp);
                       
 insert into Orders (orderNumber, price, greetingCard, color, dOrder, shop, date, orderDate) values (default, 50, 'Hello', 'Red', 'Valentine\'s roses', 'Haifa', now(), now());
 
-insert into Orders (price, greetingCard, color, dOrder, shop, date, orderDate) values (503, 'bye', 'Blue', 'Bridal\'s roses', 'Karmiel', now(), now());
+insert into Orders (price, greetingCard, color, dOrder, shop, date, orderDate) values (94.9, 'refresh', 'Yellow', 'blaaaa', 'nazareth', now(), now());
 
 SELECT * FROM Orders;
