3b8f16ea82ac2177f063f7a26d165251419160c6
Hadi <hadiDanial@users.noreply.github.com>
2022-05-09 14:54:11 +0300

    SQL Queries for creating Users, User_Orders tables

--- OrderQueries.sql
+++ OrderQueries.sql
@@ -12,3 +12,18 @@ insert into Orders (orderNumber, price, greetingCard, color, dOrder, shop, date,
 insert into Orders (price, greetingCard, color, dOrder, shop, date, orderDate) values (94.9, 'refresh', 'Yellow', 'blaaaa', 'nazareth', now(), now());
 
 SELECT * FROM Orders;
+
+DROP TABLE IF EXISTS `Users`;
+create table `Users`(`userId` int primary key AUTO_INCREMENT, `username` varchar(20) NOT NULL UNIQUE, `password` varchar(64) NOT NULL, 
+					  `firstName` varchar(20) NOT NULL,`lastName` varchar(20) NOT NULL, `emailAddress` varchar(32) NOT NULL, `phoneNumber` varchar(20) NOT NULL,
+                      `role` varchar(30) NOT NULL, `status` varchar(20) NOT NULL, `credit` float default 0, `isLoggedIn` Boolean, `lastLoginDate` timestamp);
+                      
+insert into Users (username, password, firstName, lastName, emailAddress, phoneNumber, role, status) values ('Hadi','bestpassword123','Hadi','Danial','hadi@gmail.com','05223113','CEO','Frozen');
+Update Users SET isLoggedIn=true, lastLoginDate=now() WHERE username='Hadi';
+
+DROP TABLE IF EXISTS `User_Orders`;
+create table `User_Orders`(`userId` int NOT NULL, `orderNumber` int NOT NULL, PRIMARY KEY(userId, orderNumber),
+		FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE, 
+        FOREIGN KEY (orderNumber) REFERENCES Orders(orderNumber) ON DELETE CASCADE) ;
+        
+insert into User_Orders (userId, orderNumber) values (1, 5);
