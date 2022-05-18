c8c2a75016bc687d6800f8aa7bb3d700a4d26fce
Hadi <hadiDanial@users.noreply.github.com>
2022-05-18 03:33:15 +0300

    Added User to Request, added more request types, added allowed roles per request

--- ZLI_Shared/src/requests/RequestType.java
+++ ZLI_Shared/src/requests/RequestType.java
@@ -1,9 +1,46 @@
 package requests;
 
+import enums.UserRole;
+
 public enum RequestType
 {
-	Login,
-	Logout,
-	GetAllOrders,
-	UpdateOrder
+	// Users
+	Login(UserRole.values()),
+	Logout(UserRole.values()),
+	Register(new UserRole[] {UserRole.BranchManager}),
+	
+	// Orders
+	CreateOrder(new UserRole[] {UserRole.BranchManager, UserRole.Customer}),
+	GetOrder(new UserRole[] {UserRole.BranchManager, UserRole.BranchEmployee, UserRole.Customer}),
+	GetAllOrders(new UserRole[] {UserRole.CEO }),
+	GetPendingOrders(new UserRole[] {UserRole.BranchManager, UserRole.BranchEmployee}),
+	UpdateOrderStatus(new UserRole[] {UserRole.BranchManager, UserRole.DeliveryPerson, UserRole.BranchEmployee}),
+	UpdateOrder(new UserRole[] {UserRole.BranchManager }),
+	
+	// Products/Catalog
+	GetAllProducts(UserRole.values()),
+	GetCatalogByBranch(UserRole.values());
+
+	private UserRole[] permittedRoles;
+
+	RequestType(UserRole[] permittedRoles)
+	{
+		this.permittedRoles = permittedRoles;
+	}
+	
+	/**
+	 * Is the current user role allowed to perform this function?
+	 * 
+	 * @param roleToCheck Role of the current user.
+	 * @return True if user has permission to perform the function, otherwise false.
+	 */
+	public boolean isAuthorized(UserRole roleToCheck)
+	{
+		for (UserRole role : permittedRoles)
+		{
+			if (roleToCheck == role)
+				return true;
+		}
+		return false;
+	}
 }
