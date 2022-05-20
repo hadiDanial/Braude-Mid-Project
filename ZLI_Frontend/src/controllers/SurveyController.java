c83635f8d950f1e81ca09b50e8de16f2c962686c
Amr Kal <a.k251198@hotmail.com>
2022-05-20 19:55:02 +0300

    Survey Controller

--- ZLI_Frontend/src/controllers/SurveyController.java
+++ ZLI_Frontend/src/controllers/SurveyController.java
@@ -1,11 +1,42 @@
 package controllers;
 
+import java.util.ArrayList;
+
 import entities.surveys.*;
 import entities.users.User;
+import requests.Request;
+import requests.RequestType;
+import utility.IResponse;
 
 public class SurveyController
 {
 
+	private static SurveyController instance;
+	private ClientController clientController;
+		
+	private SurveyController() {
+		clientController = ClientController.getInstance();
+	}
+
+	public static SurveyController getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new SurveyController();
+		}
+		return instance;
+	}
+	public void createSurvey(IResponse<ArrayList<Survey>> response)
+	{
+		Request request = new Request(RequestType.Survey, null);
+		clientController.sendRequest(request, response);
+	}
+	
+	public void updateOrder(IResponse<Boolean> response, Survey updatedOrder)
+	{
+		Request request = new Request(RequestType.UpdateOrder, updatedOrder);
+		clientController.sendRequest(request, response);
+	}
 	/**
 	 * 
 	 * @param survey
@@ -38,5 +69,4 @@ public class SurveyController
 		// TODO - implement SurveyController.addSurveyAnswers
 		throw new UnsupportedOperationException();
 	}
-
 }
