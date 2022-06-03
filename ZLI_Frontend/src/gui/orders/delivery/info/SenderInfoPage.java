67b3914ac92d46c79cbc7362cf3ef7ebfdb0517b
Hadi <hadiDanial@users.noreply.github.com>
2022-06-03 03:26:39 +0300

    SenderInfoPage

--- ZLI_Frontend/src/gui/orders/delivery/info/SenderInfoPage.java
+++ ZLI_Frontend/src/gui/orders/delivery/info/SenderInfoPage.java
@@ -3,13 +3,20 @@ package gui.orders.delivery.info;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import gui.guimanagement.GUIController;
+import controllers.OrderController;
+import entities.users.Order;
+import gui.guimanagement.ButtonAnimator;
+import gui.guimanagement.FormController;
+import gui.guimanagement.GUIPages;
+import gui.guimanagement.SceneManager;
+import gui.guimanagement.forms.ValidatedControl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class SenderInfoPage extends GUIController
+public class SenderInfoPage extends FormController
 {
 
     @FXML
@@ -20,20 +27,32 @@ public class SenderInfoPage extends GUIController
 
     @FXML
     private TextArea greetingInput;
+    
+    @FXML
+	private Button backBtn;
 
+	@FXML
+	private Button nextBtn;
+
+	private Order order;
+	
+	
     @FXML
     void onBackBtn(ActionEvent event) {
-
+		SceneManager.loadPreviousPage();
     }
 
     @FXML
     void onNextBtn(ActionEvent event) {
-
+    	order.setGreetingCard(greetingInput.getText());
+    	SceneManager.loadNewScene(GUIPages.CHECKOUT_DELIVERY, true);
     }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
+		setupFormController(null, nextBtn);
+		this.order = OrderController.getInstance().getOrder();
+		ButtonAnimator.addButtonAnimations(backBtn, nextBtn);
 	}
-
 }
