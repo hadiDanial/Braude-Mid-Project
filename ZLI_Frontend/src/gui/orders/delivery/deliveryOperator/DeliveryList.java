ea463b0cc7d19d572ef7a201a1abc144784e3c74
Amr Kal <a.k251198@hotmail.com>
2022-06-05 20:05:08 +0300

    Discount Management GUIPages Update

---
+++ ZLI_Frontend/src/gui/orders/delivery/deliveryOperator/DeliveryList.java
@@ -0,0 +1,39 @@
+package gui.orders.delivery.deliveryOperator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+
+public class DeliveryList {
+
+    @FXML
+    private TreeTableView<?> discountsTable;
+
+    @FXML
+    private TreeTableColumn<?, ?> orderIDColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> recipientColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> addressColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> deliveryDateColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> numOfItemsColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> preiceColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> confirmColumn;
+
+    @FXML
+    void onBackBtn(ActionEvent event) {
+
+    }
+
+}
---
+++ ZLI_Frontend/src/gui/orders/delivery/deliveryOperator/DeliveryOperatorPortal.java
@@ -0,0 +1,23 @@
+package gui.orders.delivery.deliveryOperator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DeliveryOperatorPortal {
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label jobTitleLabel;
+
+    @FXML
+    private Label branchLabel;
+
+    @FXML
+    void onInsertSurveyAnalysisBtn(ActionEvent event) {
+
+    }
+
+}
