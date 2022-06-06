12b3afa003b7123bfc700382b2bb55ad62b080a9
Yosef Awad <yosef.awad.1@gmail.com>
2022-06-06 03:25:05 +0300

    added branch manager

---
+++ ZLI_Frontend/src/gui/newPages/branchManager/BranchManagerPortal.java
@@ -0,0 +1,33 @@
+package gui.branchManager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class BranchManagerPortal {
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label jobTitleLabel;
+
+    @FXML
+    private Label branchLabel;
+
+    @FXML
+    void onViewCustomersBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewOpenOrdersBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewReportsBtn(ActionEvent event) {
+
+    }
+
+}
---
+++ ZLI_Frontend/src/gui/newPages/branchManager/reports/OrdersReportPage.java
@@ -0,0 +1,52 @@
+package gui.branchManager.reports;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+public class OrdersReportPage {
+
+    @FXML
+    private MenuButton quarterDropDown;
+
+    @FXML
+    private Label quarterErrLabel;
+
+    @FXML
+    private TextField yearField;
+
+    @FXML
+    private Label yearErrLabel;
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    void onBackBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSetQuarterBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewComplaintsReportsBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewOrdersReportsBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewSalesReportsBtn(ActionEvent event) {
+
+    }
+
+}
