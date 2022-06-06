5b190beaa966de0c9408b19853e12ee86a6f91d0
razidal <razi.dallasheh@gmail.com>
2022-06-06 21:33:09 +0300

    Portals

--- ZLI_Frontend/src/gui/users/loginPortals/CEOPortal.java
+++ ZLI_Frontend/src/gui/users/loginPortals/CEOPortal.java
@@ -4,17 +4,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import gui.guimanagement.GUIController;
+import gui.guimanagement.GUIPages;
+import gui.guimanagement.SceneManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class CEOPortal extends GUIController
 {
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources)
-	{
-	}
+    private User worker;
+	private UserController userController;
+	
     @FXML
     private Label nameLabel;
 
@@ -22,10 +23,14 @@ public class CEOPortal extends GUIController
     private Label jobTitleLabel;
 
     @FXML
-    private Label branchLabel;
-
+    private Button reviewReportsBtn;
     @FXML
     void onViewReportsBtn(ActionEvent event) {
-
+        SceneManager.loadNewScene(GUIPages.VIEW_REPORTS, saveToHistory);
     }
+    @Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+
+	}
 }
--- ZLI_Frontend/src/gui/users/loginPortals/ServiceSpecialistPortal.java
+++ ZLI_Frontend/src/gui/users/loginPortals/ServiceSpecialistPortal.java
@@ -3,14 +3,28 @@ package gui.users.loginPortals;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import controllers.BranchController;
+import controllers.UserController;
+import entities.other.Branch;
+import entities.users.User;
+import gui.guimanagement.ButtonAnimator;
 import gui.guimanagement.GUIController;
+import gui.guimanagement.GUIPages;
+import gui.guimanagement.SceneManager;
+import gui.surveys.specialist.AddAnalysis;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import utility.IResponse;
 
 public class ServiceSpecialistPortal extends GUIController
 {
-
+    private User worker;
+	private Branch workerBranch;
+	private UserController userController;
+	private BranchController branchController;
+    
     @FXML
     private Label nameLabel;
 
@@ -20,14 +34,33 @@ public class ServiceSpecialistPortal extends GUIController
     @FXML
     private Label branchLabel;
 
+    @FXML 
+    private Button insertSurveyAnalysisBtn;
+
     @FXML
     void onInsertSurveyAnalysisBtn(ActionEvent event) {
-
+        SceneManager.loadNewScene(GUIPages.ADD_SURVEY_ANALYSIS,true);
     }
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
+        userController = UserController.getInstance();
+		branchController = BranchController.getInstance();
+		worker = userController.getLoggedInUser();
+		nameLabel.setText(worker.getFullName());
+		branchController.getWorkerBranch(worker.getUserId(), new IResponse<Branch>()
+		{
+
+			@Override
+			public void executeAfterResponse(Object message)
+			{
+				workerBranch = (Branch) message;
+				branchLabel.setText(workerBranch.getBranchName());
+			}
+		});
+
+		ButtonAnimator.addButtonAnimations(insertSurveyAnalysisBtn);
 	}
 
 }
