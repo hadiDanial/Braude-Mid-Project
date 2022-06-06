233b99d88f9688717c985933dee312ea7144b997
Yosef Awad <yosef.awad.1@gmail.com>
2022-06-06 23:08:35 +0300

    added complaints handeling page

---
+++ ZLI_Frontend/src/gui/complaints/ComplaintHandling.java
@@ -0,0 +1,33 @@
+package gui.complaints;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
+public class ComplaintHandling {
+
+    @FXML
+    private Label customerIDLabel;
+
+    @FXML
+    private Text complaintNoteText;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button resolveBtn;
+
+    @FXML
+    void onBackBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onResolveBtn(ActionEvent event) {
+
+    }
+
+}
