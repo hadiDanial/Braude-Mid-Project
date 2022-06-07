a198fd381e7c231400a39dbb825d3a434d1b1dcc
Hadi <hadiDanial@users.noreply.github.com>
2022-06-07 05:58:27 +0300

    Update DeliveryList.java

--- ZLI_Frontend/src/gui/orders/delivery/deliveryOperator/DeliveryList.java
+++ ZLI_Frontend/src/gui/orders/delivery/deliveryOperator/DeliveryList.java
@@ -14,15 +14,19 @@ import entities.other.Branch;
 import entities.other.Location;
 import entities.users.Delivery;
 import entities.users.Order;
+import enums.OrderStatus;
 import gui.guimanagement.GUIController;
 import gui.guimanagement.SceneManager;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -64,7 +68,7 @@ public class DeliveryList extends GUIController{
     private TableColumn<Delivery, Float> preiceColumn;
 
     @FXML
-    private TableColumn<Delivery,Boolean > confirmColumn;
+    private TableColumn<Delivery, Delivery> confirmColumn;
 
     @FXML
     void onBackBtn(ActionEvent event) {
@@ -104,7 +108,44 @@ public class DeliveryList extends GUIController{
         deliveryDateColumn.setCellValueFactory(new PropertyValueFactory<Delivery,String >("FormattedDeliveryDate"));
         numOfItemsColumn.setCellValueFactory(new PropertyValueFactory<Delivery,Integer >("ItemsCount")); 
         preiceColumn.setCellValueFactory(new PropertyValueFactory<Delivery,Float >("Price"));
-        confirmColumn.setCellValueFactory(new PropertyValueFactory<Delivery,Boolean >("DiscountValue"));  
-    }
+        confirmColumn.setCellValueFactory(new PropertyValueFactory<Delivery,Delivery >("DiscountValue")); 
 
+        confirmColumn.setCellValueFactory(param -> new ReadOnlyObjectWrapper<Delivery>(param.getValue()));
+        confirmColumn.setCellFactory(param -> new TableCell<Delivery, Delivery>()
+		{
+			private final Button acceptButton = new Button("Mark Delivered");
+
+			@Override
+			protected void updateItem(Delivery delivery, boolean empty)
+			{
+				super.updateItem(delivery, empty);
+
+				if (delivery == null)
+				{
+					setGraphic(null);
+					return;
+				}
+				setGraphic(acceptButton);
+				acceptButton.setOnAction(event -> {
+					updateStatus(delivery);
+				});
+			}
+
+			
+		});
+    }
+    private void updateStatus(Delivery delivery)
+	{
+		IResponse<Boolean> response = new IResponse<Boolean>() {
+
+			@Override
+			public void executeAfterResponse(Object message)
+			{
+				if((Boolean) message)
+				{
+					deliveryList.remove(delivery);
+				}
+			}};
+//			orderController.updateOrderStatus(response, delivery.getOrderId(), status);
+	}
 }
